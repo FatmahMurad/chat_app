945ed27797d0f5da1804bf44b5379d1e2d404378
FatmahMurad <its.fmurad@gmail.com>
2024-01-20 01:50:50 +0300

    final

--- lib/app/modules/one_to_one_chat/domain/repo/message_repo.dart
+++ lib/app/modules/one_to_one_chat/domain/repo/message_repo.dart
@@ -38,4 +38,39 @@ class MessagingRepository {
   String _getChatRoomId(String user1, String user2) {
     return user1.hashCode <= user2.hashCode ? '$user1-$user2' : '$user2-$user1';
   }
+
+  // Retrieve messages between two users
+  Stream<List<Message>> messagesStream({
+    required String senderId,
+    required String receiverId,
+  }) {
+    try {
+      String chatRoomId = _getChatRoomId(senderId, receiverId);
+
+      final Stream<QuerySnapshot<Map<String, dynamic>>> snapshot = _firestore
+          .collection('chat_rooms')
+          .doc(chatRoomId)
+          .collection('messages')
+          .orderBy('timestamp', descending: true)
+          .snapshots();
+
+      return snapshot.map((querySnapshot) => querySnapshot.docs.map((doc) {
+            final data = doc.data();
+
+            DateTime timestamp = data['timestamp'] is int
+                ? DateTime.fromMillisecondsSinceEpoch(data['timestamp'] as int)
+                : data['timestamp'] as DateTime;
+
+            return Message(
+              messageId: doc.id,
+              senderId: data['senderId'],
+              receiverId: data['receiverId'],
+              message: data['message'],
+              timestamp: timestamp,
+            );
+          }).toList());
+    } catch (e) {
+      throw ('Error fetching messages: $e');
+    }
+  }
 }
